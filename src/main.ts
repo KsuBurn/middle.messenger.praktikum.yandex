3b61c6bdc7ef9923a3306576833131204202581d
Kseniia Burnashova <k.burnashova@lenvendo.ru>
2024-06-24 16:29:47 +0300

    added typescript

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import * as Pages from './pages';
 import * as Components from './components';
 import { profileState } from './pages/Profile';
 
-const pages = {
+type PagesType = {
+    [key: string]: [string, args?: any];
+}
+const pages: PagesType = {
     'start': [Pages.StartPage],
     'sign-in': [ Pages.SignIn ],
     'sign-up': [ Pages.SignUp ],
@@ -17,18 +20,18 @@ Object.entries(Components).forEach(([ name, component ]) => {
     Handlebars.registerPartial(name, component);
 });
 
-function navigate(page) {
+function navigate(page: string): void {
     const [source, args] = pages[page];
     document.body.innerHTML = Handlebars.compile(source)(args);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (): void => {
     const pathName = window.location.pathname === '/' ? 'start' : window.location.pathname.replace('/', '');
     navigate(pathName);
 });
 
-document.addEventListener('click', e => {
-    const page = e.target.getAttribute('page');
+document.addEventListener('click', (e: Event): void => {
+    const page = (e.target as HTMLElement)?.getAttribute('page');
     if (page) {
         navigate(page);
 
